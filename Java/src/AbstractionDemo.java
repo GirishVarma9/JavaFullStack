7626933c42c89b1a007f56cbc615de4118e1e3a2
GirishVarma9 <116869226+GirishVarma9@users.noreply.github.com>
2024-04-07 15:01:04 +0530

    ALL JAVA FILES 07/04/2024

--- Java/src/AbstractionDemo.java
+++ Java/src/AbstractionDemo.java
@@ -25,7 +25,7 @@ class Bike extends Cycle {
     //when we extend the abstract class we should have to implement the undefined methods of abstract class
     @Override
     public void handle() {
-        System.out.println("we use handle to change the directions");
+        System.out.println("we use handle to changegit directions");
     }
 
     public void lights() {
@@ -43,7 +43,7 @@ public class AbstractionDemo {
         int d = x.atlasCycle;
         int w = x.wheels(2);
 
-        // if there is nothing to return we just have to call the methods but we cannot print the methods
+        // if there is nothing to return we just have to call the methods but we cannot print the methods.
         x.handle();
         x.lights();
 
